efcd41923900637fc3e2770905520928210f8e74
Jing <122576772+DaiJing11@users.noreply.github.com>
2023-03-27 09:50:25 -0400

    Create cloudwatch.tf

---
+++ cloudwatch.tf
@@ -0,0 +1,58 @@
+# create IAM policy
+resource "aws_iam_policy" "cloudWatch" {
+  name        = "WebAppS3"
+  path        = "/"
+  description = "IAM S3 policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:DeleteObject",
+          "s3:DeleteObjectVersion",
+          "s3:ListBucket",
+          "s3:ListBucketMultipartUploads"
+        ],
+        "Effect" : "Allow",
+        "Resource" : [
+          "arn:aws:s3:::${aws_s3_bucket.new_bucket.bucket}",
+          "arn:aws:s3:::${aws_s3_bucket.new_bucket.bucket}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# create IAM role
+resource "aws_iam_role" "EC2-CSYE6225" {
+  name        = "EC2-CSYE6225"
+  description = "test - access to source and destination S3 bucket"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "EC2-CSYE6225" {
+  role       = aws_iam_role.EC2-CSYE6225.name
+  policy_arn = aws_iam_policy.WebAppS3.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.EC2-CSYE6225.name
+}
