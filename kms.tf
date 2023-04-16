1cee11bdf10513b24a60ac0701cb294675e1fc09
Jing <122576772+DaiJing11@users.noreply.github.com>
2023-04-16 15:43:27 -0400

    Update kms.tf

--- kms.tf
+++ kms.tf
@@ -1,7 +1,7 @@
 
 resource "aws_kms_key" "kms_ebs" {
   description             = "KMS key for ebs"
-  deletion_window_in_days = 30
+  deletion_window_in_days = 7
 }
 
 resource "aws_kms_key_policy" "kms_policy_ebs" {
@@ -9,43 +9,124 @@ resource "aws_kms_key_policy" "kms_policy_ebs" {
   policy = jsonencode({
     Id = "kms_policy_ebs"
     Statement = [
+      # {
+      #   "Sid": "Enable ebs Permissions_${timestamp()}",
+      #   "Effect": "Allow",
+      #   "Principal": {
+      #     "AWS": [
+      #       "arn:aws:iam::100248794926:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+      #     ]
+      #   },
+      #   "Action": [
+      #     "kms:Create*",
+      #     "kms:Describe*",
+      #     "kms:Enable*",
+      #     "kms:List*",
+      #     "kms:Put*",
+      #     "kms:Update*",
+      #     "kms:Revoke*",
+      #     "kms:Disable*",
+      #     "kms:Get*",
+      #     "kms:Delete*",
+      #     "kms:TagResource",
+      #     "kms:UntagResource",
+      #     "kms:Encrypt",
+      #     "kms:Decrypt",
+      #     "kms:ReEncrypt*",
+      #     "kms:GenerateDataKey*",
+      #     "kms:DescribeKey",
+      #   ],
+      #   "Resource": "*"
+      # },
+      # {
+      #   "Sid": "Allow attachment of persistent resources",
+      #   "Effect": "Allow",
+      #   "Principal": {
+      #     "AWS": [
+      #       "arn:aws:iam::100248794926:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+      #     ]
+      #   },
+      #   "Action": [
+      #     "kms:CreateGrant"
+      #   ],
+      #   "Resource": "*",
+      #   "Condition": {
+      #     "Bool": {
+      #       "kms:GrantIsForAWSResource": true
+      #     }
+      #   }
+      # }, {
+      #   "Sid": "Allow IAM roles to update the key policy",
+      #   "Effect": "Allow",
+      #   "Principal": {
+      #     "AWS": [
+      #       "arn:aws:iam::100248794926:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+      #     ]
+      #   },
+      #   "Action": [
+      #     "kms:PutKeyPolicy",
+      #     "kms:UpdateKeyPolicy"
+      #   ],
+      #   "Resource": aws_kms_key.kms_ebs.arn,
+      # },
+
       {
-        "Sid": "Enable IAM User Permissions",
-        "Effect": "Allow",
-        "Principal": {
-          "AWS": [
-            "arn:aws:iam::100248794926:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
-            "${aws_iam_role.EC2-CSYE6225.arn}"
-          ]
+        "Sid" : "Enable IAM User Permissions",
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" = "*"
         },
-        "Action": [
-          "kms:Encrypt",
-          "kms:Decrypt",
-          "kms:ReEncrypt*",
-          "kms:GenerateDataKey*",
-          "kms:DescribeKey"
+        "Action" : [
+          "kms:*"
         ],
-        "Resource": "*"
+        "Resource" : "*"
       },
+      # {
+      #   "Sid" : "Allow service-linked role use of the customer managed key",
+      #   "Effect" : "Allow",
+      #   "Principal" : {
+      #     "AWS" : [
+      #       "arn:aws:iam::100248794926:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+      #     ]
+      #   },
+      #   "Action" : [
+      #     "kms:Encrypt",
+      #     "kms:Decrypt",
+      #     "kms:ReEncrypt*",
+      #     "kms:GenerateDataKey*",
+      #     "kms:DescribeKey"
+      #   ],
+      #   "Resource" : "*"
+      # },
       {
-        "Sid": "Allow attachment of persistent resources",
-        "Effect": "Allow",
-        "Principal": {
-          "AWS": [
-            "arn:aws:iam::100248794926:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
-            "${aws_iam_role.EC2-CSYE6225.arn}"
-          ]
-        },
-        "Action": [
-          "kms:CreateGrant"
-        ],
-        "Resource": "*",
-        "Condition": {
-          "Bool": {
-            "kms:GrantIsForAWSResource": true
-          }
+            "Sid": "Enable IAM User Permissions_${timestamp()}",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::100248794926:root"
+            },
+            "Action": [
+                "kms:Create*",
+                "kms:Describe*",
+                "kms:Enable*",
+                "kms:List*",
+                "kms:Put*",
+                "kms:Update*",
+                "kms:Revoke*",
+                "kms:Disable*",
+                "kms:Get*",
+                "kms:Delete*",
+                "kms:TagResource",
+                "kms:UntagResource",
+                "kms:Encrypt",
+                "kms:Decrypt",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:DescribeKey",
+                "kms:PutKeyPolicy",
+                "kms:UpdateKeyPolicy"
+            ],
+            "Resource": "*"
         }
-      },
     ]
     Version = "2012-10-17"
   })
@@ -54,7 +135,7 @@ resource "aws_kms_key_policy" "kms_policy_ebs" {
 
 resource "aws_kms_key" "kms_rds" {
   description             = "KMS key for rds"
-  deletion_window_in_days = 30
+  deletion_window_in_days = 7
 }
 
 resource "aws_kms_key_policy" "kms_policy_rds" {
@@ -63,7 +144,7 @@ resource "aws_kms_key_policy" "kms_policy_rds" {
     Id = "kms_policy_rds"
     Statement = [
       {
-        Sid = "Enable IAM User Permissions"
+        Sid    = "Enable IAM User Permissions_${timestamp()}"
         Effect = "Allow"
         Principal = {
           AWS = "*"
@@ -85,7 +166,7 @@ resource "aws_kms_key_policy" "kms_policy_rds" {
         Resource = "*"
       },
       {
-        Sid = "Allow access to RDS instance"
+        Sid    = "Allow access to RDS instance"
         Effect = "Allow"
         Principal = {
           Service = "rds.amazonaws.com"
